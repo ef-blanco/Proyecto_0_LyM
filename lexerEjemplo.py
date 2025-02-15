73790dd97ce0ac5feba4532fd9efcf41808efe28
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-02-15 11:24:20 -0500

    Correcciones realizadas

--- lexerEjemplo.py
+++ lexerEjemplo.py
@@ -40,6 +40,7 @@ def prueba (archivo): #Este método devuelve un solo string con todas las lineas
             lista_con_codigo.append(lin_cod)
             
     print("lista_cod: ",lista_con_codigo)
+    archivo.close()
     
     definicion_var = [] #Esta lista contendra las lista que contengan el caracter '|', que seran los casos vinculados a la declaracion de variables
     
@@ -60,18 +61,18 @@ def prueba (archivo): #Este método devuelve un solo string con todas las lineas
     for i in listas_con_solo_corcheteR:
 
         if lista_con_codigo[i-1] != ["]"]:
-            lista_con_codigo[i-1].extend(lista_con_codigo[i])
+            lista_con_codigo[i-1].extend(lista_con_codigo[i]) #Añade el ']' a la linea anterior
         else:
             k = 1
             stop = False
-            while ((i-k)>0)and(not stop):
+            while ((i-k)>0)and(not stop): #Se usa para añadir el ']' a la linea anterior que no sea igual a [']']
                 if lista_con_codigo[i-k] != ["]"]:
                     stop =True
                     lista_con_codigo[i-k].extend(lista_con_codigo[i])
                 k+=1
                
     elementos_borrar = len(listas_con_solo_corcheteR)
-    while elementos_borrar>0:
+    while elementos_borrar>0: #Elimina todas las ocurrencias de ']' que se hayan unido a otras listas
         lista_con_codigo.remove(["]"])
         elementos_borrar-=1
         
@@ -84,17 +85,12 @@ def prueba (archivo): #Este método devuelve un solo string con todas las lineas
         if 'proc' in lista_con_codigo[l]:
             listas_con_proc.append(l)
         l+=1
-        
-    #for lista in lista_con_codigo:
-        #if 'proc' in lista:
-            #listas_con_proc.append(lista_con_codigo.index(lista))
     
     elementos_borrar = [] #Esta lista indica que elementos se eliminaran luego de juntar todas las lineas de un procedimiento
     
     for i in listas_con_proc:
-        j = i
+        j = i+1
         corchete_detectado = False
-        nuevalista = []
         
         while (j<len(lista_con_codigo))and(not corchete_detectado):
             
@@ -102,16 +98,15 @@ def prueba (archivo): #Este método devuelve un solo string con todas las lineas
                 corchete_detectado = True #Aun es necesario incluir el caso en el que hay subbloques dentro de un proc, el ciclo se detendría al ver el primer ']'
                 
             elementos_borrar.append(lista_con_codigo[j])
-            nuevalista.extend(lista_con_codigo[j])
+            lista_con_codigo[i].extend(lista_con_codigo[j])
             j+=1
         
-        lista_con_codigo.insert(i,nuevalista) #Inserta en la lista el procedimiento en la pos de la linea en la que se empezo
         
     for elem in elementos_borrar: #Se eliminan las listas que fueron juntadas con otras
         lista_con_codigo.remove(elem)
     
     
-    #CORREGIR
+    #CORREGIDO
     #Se unen los '[' que se encuentran solo en una linea a la linea siguiente
     
     listas_con_solo_corcheteL = [] #Lista con indices de Listas con solo "["
@@ -122,28 +117,37 @@ def prueba (archivo): #Este método devuelve un solo string con todas las lineas
             listas_con_solo_corcheteR.append(l)
         l+=1
             
-    elementos_borrar = [] #Esta lista indica que elementos se eliminaran luego de juntar todos los '[' solos con su linea siguiente
-    
     for i in listas_con_solo_corcheteL:
-        nueva_lista = lista_con_codigo[i][:] #Se copia el '[' en nueva_lista
-        nueva_lista.extend(lista_con_codigo[i+1]) #Se le añade a la lista lo que estaba en la linea siguiente
-        elementos_borrar.append(lista_con_codigo[i])
-        elementos_borrar.append(lista_con_codigo[i+1])
-        lista_con_codigo.insert(i,nueva_lista) #Se añade la nueva lista a la posicion donde estaba la linea antes del ']'
-    
-    for elem in elementos_borrar: #Se eliminan las listas que fueron juntadas con otras
-        lista_con_codigo.remove(elem)
         
+        if lista_con_codigo[i+1] != ["["]: #De la misma manera que con los ']' se juntan los '[' con su siguiente linea
+            lista_con_codigo[i+1].insert(0,"[")
+        else:
+            k = 1
+            stop = False
+            while ((i+k)<len(lista_con_codigo))and(not stop): #Se usa para añadir el '[' a la siguiente linea distinta de '['
+                if lista_con_codigo[i+k] != ["["]:
+                    stop =True
+                    lista_con_codigo[i+k].insert(0,"[")
+                k+=1
+    
+    elementos_borrar = len(listas_con_solo_corcheteL)
+    while elementos_borrar>0: #Elimina todas las ocurrencias de '[' añadidas a otras listas
+        lista_con_codigo.remove(["["])
+        elementos_borrar-=1
+    
+    
+    print("\n")
     print("CONTENIDO: ",lista_con_codigo)        
     
-    for lista in lista_con_codigo:
+    for lista in lista_con_codigo: #Separa las listas de acuerdo a su tipo
         if 'proc' in lista:
             procedimientos.append(lista)
         elif '|' in lista:
             definicion_var.append(lista)
     
-    
+    print("\n")
     print("DEF_VAR: ",definicion_var)
+    print("\n")
     print("PROCS: ",procedimientos)
         
 prueba("prueba.txt")
