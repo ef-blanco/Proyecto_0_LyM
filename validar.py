e6df999ad61c2afd6bb083231f349ab6e57c69a0
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-02-17 20:18:03 -0500

    Cambios en validar_bloque

--- validar.py
+++ validar.py
@@ -20,7 +20,7 @@ PALABRAS_RESERVADAS = ["|","[","]","#chips", "#balloons","facing", "canPut", "ca
 
 # Verificar cada instrucción o cada variable caracter por caracter
 
-def verificar_goto(tokens,i):
+def verificar_goto(tokens,i)->bool:
     if not(tokens[0] == "goto"):
         return False
     elif not(tokens[1] == ":"):
@@ -38,7 +38,7 @@ def verificar_goto(tokens,i):
     else:
         return True
 
-def verificar_move(tokens,i):
+def verificar_move(tokens,i)->bool:
     if not(tokens[0] == "move"):
         return False
     elif not(tokens[1] == ":"):
@@ -50,7 +50,7 @@ def verificar_move(tokens,i):
     else:
         return True
     
-def verificar_turn(tokens,i):
+def verificar_turn(tokens,i)->bool:
     if not(tokens[0] == "turn"):
         return False
     elif not(tokens[1] == ":"):
@@ -62,7 +62,7 @@ def verificar_turn(tokens,i):
     else:
         return True
     
-def verificar_face(tokens, i):
+def verificar_face(tokens, i)->bool:
     if not(tokens[0] == "face"):
         return False
     elif not(tokens[1] == ":"):
@@ -74,7 +74,7 @@ def verificar_face(tokens, i):
     else:
         return True
 
-def verificar_putOfType(tokens,i):
+def verificar_putOfType(tokens,i)->bool:
     if not(tokens[0] == "put"):
         return False
     elif not(tokens[1] == ":"):
@@ -92,7 +92,7 @@ def verificar_putOfType(tokens,i):
     else:
         return True
     
-def verificar_pickOfType(tokens,i):
+def verificar_pickOfType(tokens,i)->bool:
     if not(tokens[0] == "pick"):
         return False
     elif not(tokens[1] == ":"):
@@ -110,7 +110,7 @@ def verificar_pickOfType(tokens,i):
     else:
         return True
     
-def verificar_moveToThe(tokens,i): #Cambio: el nombre de la función era modeToThe
+def verificar_moveToThe(tokens,i)->bool: #Cambio: el nombre de la función era modeToThe
     if not(tokens[0] == "move"):
         return False
     elif not(tokens[1] == ":"):
@@ -128,7 +128,7 @@ def verificar_moveToThe(tokens,i): #Cambio: el nombre de la función era modeToT
     else:
         return True
     
-def verificar_moveInDir(tokens,i):
+def verificar_moveInDir(tokens,i)->bool:
     if not(tokens[0] == "move"):
         return False
     elif not(tokens[1] == ":"):
@@ -146,7 +146,7 @@ def verificar_moveInDir(tokens,i):
     else:
         return True
     
-def verificar_jumpToThe(tokens,i):
+def verificar_jumpToThe(tokens,i)->bool:
     if not(tokens[0] == "jump"):
         return False
     elif not(tokens[1] == ":"):
@@ -164,7 +164,7 @@ def verificar_jumpToThe(tokens,i):
     else:
         return True
     
-def verificar_jumpInDir(tokens,i):
+def verificar_jumpInDir(tokens,i)->bool:
     if not(tokens[0] == "jump"):
         return False
     elif not(tokens[1] == ":"):
@@ -182,7 +182,7 @@ def verificar_jumpInDir(tokens,i):
     else:
         return True
     
-def verificar_nop(tokens,i):
+def verificar_nop(tokens,i)->bool:
     if not(tokens[0] == "nop"):
         return False
     elif not(tokens[1] == "."):
@@ -192,7 +192,7 @@ def verificar_nop(tokens,i):
     
 # Verificar correctamente condiciones
 
-def verificar_facingCondicion(tokens,i):
+def verificar_facingCondicion(tokens,i)->bool:
     if not(tokens[0] == "facing"):
         return False
     elif not(tokens[1] == ":"):
@@ -204,7 +204,7 @@ def verificar_facingCondicion(tokens,i):
     else:
         return True
     
-def verificar_canPutOfType(tokens,i):
+def verificar_canPutOfType(tokens,i)->bool:
     if not(tokens[0] == "canPut"):
         return False
     elif not(tokens[1] == ":"):
@@ -222,7 +222,7 @@ def verificar_canPutOfType(tokens,i):
     else:
         return True
     
-def verificar_canPickOfType(tokens,i):
+def verificar_canPickOfType(tokens,i)->bool:
     if not(tokens[0] == "canPick"):
         return False
     elif not(tokens[1] == ":"):
@@ -240,7 +240,7 @@ def verificar_canPickOfType(tokens,i):
     else:
         return True
     
-def verificar_canMoveInDir(tokens,i):
+def verificar_canMoveInDir(tokens,i)->bool:
     if not(tokens[0] == "canMove"):
         return False
     elif not(tokens[1] == ":"):
@@ -258,7 +258,7 @@ def verificar_canMoveInDir(tokens,i):
     else:
         return True
     
-def verificar_canJumpInDir(tokens,i):
+def verificar_canJumpInDir(tokens,i)->bool:
     if not(tokens[0] == "canJump"):
         return False
     elif not(tokens[1] == ":"):
@@ -276,7 +276,7 @@ def verificar_canJumpInDir(tokens,i):
     else:
         return True
     
-def verificar_canMoveToThe(tokens,i):
+def verificar_canMoveToThe(tokens,i)->bool:
     if not(tokens[0] == "canMove"):
         return False
     elif not(tokens[1] == ":"):
@@ -294,7 +294,7 @@ def verificar_canMoveToThe(tokens,i):
     else:
         return True
     
-def verificar_canJumpToThe(tokens,i):
+def verificar_canJumpToThe(tokens,i)->bool:
     if not(tokens[0] == "canJump"):
         return False
     elif not(tokens[1] == ":"):
@@ -312,7 +312,7 @@ def verificar_canJumpToThe(tokens,i):
     else:
         return True
 
-def verificar_Not(tokens,i):
+def verificar_Not(tokens,i)->bool:
     if not(tokens[0] == "not"):
         return False
     elif not(tokens[1] == ":"):
@@ -327,7 +327,7 @@ def verificar_Not(tokens,i):
 
 # Verificación de variables, bloques y procedimientos
 
-def validar_variable(lista,i):
+def validar_variable(lista,i)->bool:
     if not(lista[0] in DVARIABLE):
         return False
     elif not(lista[-1] in DVARIABLE):
@@ -347,20 +347,124 @@ def validar_variable(lista,i):
     else:
         return True 
     
-def validar_bloque(tokens, i):
-    resultado=bool
+def validar_bloque(tokens, i)->bool:
+    resultado = True
     if not(tokens[0] in LBLOQUE) or not(tokens[-1] in RBLOQUE):
         return False
+    
+    lista_instrucciones = [] #Tendra en listas las intrucciones a revisar
+    validacion_instrucciones = True
+    punto_detectado = False
     j=1
-    while j < len(tokens)-1:
-        if tokens[j][0]=="|":
-            resultado=validar_variable(tokens[j], j)
+    while j < len(tokens)-1: #La condicion tal vez deba ser j < len(tokens)-2
+        if isinstance(tokens[j],list):
+            if tokens[j][0]=="|":
+                resultado = validar_variable(tokens[j], j) and resultado
+            
+            elif (tokens[j][0] == "[")and(tokens[j][-1] == "]"):
+                resultado = resultado and validar_bloque(tokens[j],j)
+        
+        
+        else:
+            nueva_instruc = []
+            nueva_instruc.append(tokens[j])
+            if tokens == ".":
+                punto_detectado = True
+                
+            if punto_detectado: #Si se detecta un punto significa que ya termino una instruccion
+                lista_instrucciones.append(nueva_instruc) #La instruccion se añade a la lista de instrucciones
+                nueva_instruc = []
+                punto_detectado = False
+            
         j+=1
+        
+        instruc_Tipo = {} #Guarda indice_insturccion (llave): tipo (valor)
+        for instruc in lista_instrucciones:
+            if ":=" in instruc:
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "ASIGNACION"
+            
+            elif "goto" in instruc:
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "GOTO"
+            
+            elif "move" in instruc:
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "MOVE"
+                
+            elif "turn" in instruc:
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "TURN"
+                
+            elif "face" in instruc:
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "FACE"
+            
+            elif ("put" in instruc) and ("ofType" in instruc):
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "PUT_OFTYPE"
+            
+            elif ("pick" in instruc) and ("ofType" in instruc):
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "PICK_OFTYPE"
+            
+            elif ("move" in instruc) and ("toThe" in instruc):
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "MOVE_TOTHE"
+            
+            elif ("move" in instruc) and ("inDir" in instruc):
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "MOVE_INDIR"
+            
+            elif ("jump" in instruc) and ("toThe" in instruc):
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "JUMP_TOTHE"
+            
+            elif ("jump" in instruc) and ("inDir" in instruc):
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "JUMP_INDIR"
+            
+            elif "nop" in instruc:
+                instruc_Tipo[lista_instrucciones.index(instruc)] = "NOP"
+            
+            
+            for indice in instruc_Tipo.keys(): #Se revisa cada instruccion y se va modificando un booleano que determina si todas las instrucciones fueron correctas
+                tipo = instruc_Tipo[indice]
+                match tipo:
+                    case "ASIGNACION":
+                        validacion_instrucciones = validacion_instrucciones and verificarAsignacion
+                        
+                    case "GOTO":
+                        validacion_instrucciones = validacion_instrucciones and verificar_goto(lista_instrucciones[indice],indice)
+                        
+                    case "MOVE":
+                        validacion_instrucciones = validacion_instrucciones and verificar_move(lista_instrucciones[indice],indice)
+                        
+                    case "TURN":
+                        validacion_instrucciones = validacion_instrucciones and verificar_turn(lista_instrucciones[indice],indice)
+                        
+                    case "FACE":
+                        validacion_instrucciones = validacion_instrucciones and verificar_face(lista_instrucciones[indice],indice)
+                        
+                    case "PUT_OFTYPE":
+                        validacion_instrucciones = validacion_instrucciones and verificar_canPutOfType(lista_instrucciones[indice],indice)
+                    
+                    case "PICK_OFTYPE":
+                        validacion_instrucciones = validacion_instrucciones and verificar_canPickOfType(lista_instrucciones[indice],indice)
+                    
+                    case "MOVE_TOTHE":
+                        validacion_instrucciones = validacion_instrucciones and verificar_canMoveToThe(lista_instrucciones[indice],indice)
+                        
+                    case "MOVE_INDIR":
+                        validacion_instrucciones = validacion_instrucciones and verificar_moveInDir(lista_instrucciones[indice],indice)
+                    
+                    case "JUMP_TOTHE":
+                        validacion_instrucciones = validacion_instrucciones and verificar_jumpToThe(lista_instrucciones[indice],indice)
+                    
+                    case "JUMP_INDIR":
+                        validacion_instrucciones = validacion_instrucciones and verificar_jumpInDir(lista_instrucciones[indice],indice)
+                        
+                    case "NOP":
+                        validacion_instrucciones = validacion_instrucciones and verificar_nop(lista_instrucciones[indice],indice)
+                
+    return validacion_instrucciones and resultado
+                    
+            
+            
+    
+    
+    
 
-    else:
-        return resultado
-
-def validar_procedimiento(tokens, i):
+def validar_procedimiento(tokens, i)->bool:
     result = True
     if not(tokens[0] == "proc"):
         result = False
